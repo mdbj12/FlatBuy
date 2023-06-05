10811db992878bc8003e553247c13c3184aac762
Michael Jeung <michaelj229@gmail.com>
2023-06-05 13:26:28 -0400

    routes working. added dependencies to package-lock

---
+++ client/src/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+        <div>
+            <p>
+                FLATBUY HOMEPAGE
+            </p>
+        </div>
+    )
+}
+
+export default Homepage
--- client/src/Navbar/Navbar.jsx
+++ client/src/Navbar/Navbar.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import Homepage from '../Homepage/Homepage';
+import Login from '../LogIn/Login';
+
+// const Navbar = () => {
+//     return (
+//         <nav>
+//             <div className='flex flex-wrap items-center justify-between mx-auto p4'>
+//                 <p>NAVBAR WITH LINKS</p>
+                
+//             </div>
+//         </nav>
+//     )   
+// }
+
+// export default Navbar
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <p>NAVBAR WITH LINKS</p>
-            </div>
-        </nav>
-    )   
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='Log-In' element={<Login />} />
+        </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default Navbar
