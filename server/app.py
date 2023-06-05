3354aabcbe2cf951008a3ffc374e1fbf8ad408d5
Clifton Gockul <clifga@gmail.com>
2023-06-05 12:44:41 -0400

    stuff

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 
-from models import db, Consumer, Vendor, Items
+from models import db, Consumer, Items, Cart, CartItem, Rating
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -12,7 +12,40 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+api = Api(app)
 
 @app.route('/')
 def index():
     return '<h1>HOMEPAGE</h1>'
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        user = Consumer.query.filter_by(username=username).first()
+        if user is None:
+            return make_response(jsonify({'error': 'User not found'}), 404)
+        if user.password != password:
+            return make_response(jsonify({'error': 'Invalid password'}), 401)
+        return make_response(jsonify({'message': 'Login successful'}), 200)
+api.add_resource(Login, '/login')
+
+class Create_User(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        email = data['email']
+        first_name = data['first_name']
+        last_name = data['last_name']
+        address = data['address']
+        phone_number = data['phone_number']
+        user = Consumer.query.filter_by(username=username).first()
+        if user is not None:
+            return make_response(jsonify({'error': 'Username already exists'}), 409)
+        user = Consumer(username=username, password=password, email=email, first_name=first_name, last_name=last_name, address=address, card_number=card_number)
+        db.session.add(user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'User created'}), 201)
+api.add_resource(Create_User, '/create_user')
--- server/models.py
+++ server/models.py
@@ -6,53 +6,66 @@ from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
 class Consumer(db.Model, SerializerMixin):
-    __tablename__ = 'consumer'
+    __tablename__ = 'consumers'
 
-    id = db.Column(db.Integer, primaryKey=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
     email = db.Column(db.String)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)    
     address = db.Column(db.String)
-    card_number = db.Column(db.Integer)
+    phone_number = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    pass
+    rating = db.relationship('Rating', backref='consumer')
+    cart = db.relationship('Cart', backref='consumer')
 
-class Vendor(db.Model, SerializerMixin):
-    __tablename__ = 'vendor'
+class Cart(db.Model, SerializerMixin):
+    __tablename__ = 'carts'
 
-    id = db.Column(db.Integer, primaryKey=True)
-    name = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('consumers.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    cart_items = db.relationship('CartItem', backref='cart')
 
-    pass
 
-class Items(db.Model, SerializerMixin):
+class CartItem(db.Model, SerializerMixin):
+    __tablename__ = 'cart_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('carts.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    quantity = db.Column(db.Integer)
+    # cart = db.relationship('Cart', backref='cart_items')
+    # item = db.relationship('Item', backref='cart_items')
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+class Rating(db.Model, SerializerMixin):
+    __tablename__ = 'ratings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('consumers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    rate_score = db.Column(db.Integer)
+
+
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
-    id = db.Column(db.Integer, primaryKey=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     inventory_count = db.Column(db.Integer)
     price = db.Column(db.Float)
+    rating = db.relationship('Rating', backref='item')
+    image = db.Column(db.String)
+    cart_items = db.relationship('CartItem', backref='item')
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
 
-    @validates('inventory_count')
-    def validate_inventory(self, key, inventory_count):
-        if inventory_count > 0:
-            return inventory_count
-        raise ValueError('No Existing Inventory')
-    pass
 
-class Purchase(db.Model, SerializerMixin):
-    __tablename__ = 'purchase'
-
-    id = db.Column(db.Integer, primary_key=True)
-    consumer_id = db.Column(db.Integer, db.ForeignKey('consumer.id'))
-    consumer = db.relationship('Consumer', backref='purchase')
-    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
-    item = db.relationship('Items', backref='purchase')
-    quantity = db.Column(db.Integer)
-    total_price = db.Column(db.Float)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    passh
+    
