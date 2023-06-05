276e358d9f232dc47c632796433f512dc1e6ab83
maibijiben <michaelpmaurerii@gmail.com>
2023-06-05 14:38:37 -0400

    added cart stuff

--- server/app.py
+++ server/app.py
@@ -44,8 +44,47 @@ class Create_User(Resource):
         user = Consumer.query.filter_by(username=username).first()
         if user is not None:
             return make_response(jsonify({'error': 'Username already exists'}), 409)
-        user = Consumer(username=username, password=password, email=email, first_name=first_name, last_name=last_name, address=address, card_number=card_number)
+        user = Consumer(
+            username=username, 
+            password=password, 
+            email=email, 
+            first_name=first_name, 
+            last_name=last_name, 
+            address=address, 
+            card_number=card_number
+        )
         db.session.add(user)
         db.session.commit()
         return make_response(jsonify({'message': 'User created'}), 201)
 api.add_resource(Create_User, '/create_user')
+
+class Create_Cart(Resource):
+    items = []
+    def post(self):
+        data = request.get_json()
+        user_id = data['user_id']
+        created_at = data['created_at']
+        updated_at = data['updated_at']
+        cart_items = data['cart_items']
+        new_cart = Cart(
+            created_at = created_at,
+            updated_at = updated_at,
+            cart_items = cart_items,
+            user_id = user_id
+        )
+        db.session.add(new_cart)
+        db.session.commit()
+        return make_response(jsonify({'message': "cart created"}), 201)
+api.add_resource(Create_Cart, '/create_cart')
+
+class Update_Cart(Resource, Id):
+    def patch(self, id):
+        cart_update = Cart.query.filter_by(id=id).first().to_dict()
+        data = request.get_json()
+        for attr in data:
+            setattr(cart_update, attr, data[attr])
+        db.session.add(cart_update)
+        db.session.commit()
+        return make_response(jsonify({'message': 'success, cart updated'}))
+api.add_resource(Update_Cart, '/update_cart/<int:id>')
+        
